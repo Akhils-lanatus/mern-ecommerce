623578559b1beea8ff1f36ba03a6b6e4754ba185
Akhils-lanatus <akhil.s@lanatussystems.com>
2024-06-28 10:16:36 +0530

    Changed Tax Amount

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -30,7 +30,7 @@ const Cart = () => {
   const savings =
     parseFloat((totalAmountBeforeDiscount - totalAmount)?.toFixed(2)) || 0;
   const store_pickup_price = 10.0;
-  const tax_amount = parseFloat((totalAmount * 0.05)?.toFixed(2));
+  const tax_amount = parseFloat((totalAmount * 0.02)?.toFixed(2));
   const final_amount = (
     totalAmountBeforeDiscount +
     store_pickup_price +
